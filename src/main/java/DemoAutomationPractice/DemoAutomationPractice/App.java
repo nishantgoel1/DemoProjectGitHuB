227995372f302a65f7b8ce7e48b0b6b61d179f26
nishantgoel1 <nishantg425@gmail.com>
2020-08-02 13:03:49 +0530

    My First Commit for Demo Autonation Jenkins Project

---
+++ src/main/java/DemoAutomationPractice/DemoAutomationPractice/App.java
@@ -0,0 +1,13 @@
+package DemoAutomationPractice.DemoAutomationPractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/DemoJenkingsTesting/DemoJenkins.java
@@ -0,0 +1,57 @@
+package DemoJenkingsTesting;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class DemoJenkins
+{
+	WebDriver driver;
+
+@BeforeMethod
+public void setUp() throws InterruptedException
+{
+	
+	System.setProperty("webdriver.chrome.driver","C:\\Users\\dfd\\Desktop\\Nishant_Desktop\\Selenium_Java_Assignments\\22February_Batch_Selenium\\chromedriver_win32_Latest_Version\\chromedriver.exe");
+
+	driver=new ChromeDriver();
+	driver.manage().window().fullscreen();
+
+	Thread.sleep(3000);
+
+	driver.get("https://www.amazon.com");
+	
+	
+
+}
+
+@Test
+public void mainTestMethod() throws InterruptedException
+{
+	Thread.sleep(3000);
+	
+	driver.navigate().to("https://www.youtube.com");
+	String title=driver.getTitle();
+	
+	System.out.println("Ttitle fof the Page is "+title);
+	
+	Thread.sleep(3000);
+	driver.navigate().back();
+	
+	Thread.sleep(3000);
+	String titleNew=driver.getTitle();
+	
+	System.out.println("New Ttitle of the Page is "+titleNew);
+
+}
+
+@AfterMethod
+public void tearDown()
+{
+
+	driver.close();
+
+}
+	
+}
